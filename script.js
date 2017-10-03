016801f41a9861b663cd1d9ab2ce8284cd5a540a
Tommy Howell <tommyhowell6@gmail.com>
2017-10-03 16:17:10 -0600

    basic page working

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+// &date=1998-08-28
+$.ajax({
+  url: url,
+  success: function(result){
+  if("copyright" in result) {
+    $("#copyright").text("Image Credits: " + result.copyright);
+  }
+  else {
+    $("#copyright").text("Image Credits: " + "Public Domain");
+  }
+  
+  if(result.media_type == "video") {
+    $("#apod_img_id").css("display", "none"); 
+    $("#apod_vid_id").attr("src", result.url);
+  }
+  else {
+    $("#apod_vid_id").css("display", "none"); 
+    $("#apod_img_id").attr("src", result.url);
+  }
+  $("#reqObject").text(url);
+  // $("#returnObject").text(JSON.stringify(result, null, 4));  
+  $("#apod_explaination").text(result.explanation);
+  $("#apod_title").text(result.title);
+}
+});
+});
