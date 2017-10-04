a534d5f479f7fbbda0457d2e12bdcddf36040cbc
Tommy Howell <tommyhowell6@gmail.com>
2017-10-04 00:04:25 -0600

    got the date working added a button we can put our favorite dates on

--- script.js
+++ script.js
@@ -21,23 +21,48 @@ $.ajax({
     $("#apod_img_id").attr("src", result.url);
   }
   $("#reqObject").text(url);
-  // $("#returnObject").text(JSON.stringify(result, null, 4));  
+  // $("#returnObject").text(JSON.stringify(result, null, 4));
+  $("#apod_date").text(result.date);  
   $("#apod_explaination").text(result.explanation);
   $("#apod_title").text(result.title);
 }
 });
 
 $("#submitButton").click(function(e){
-  var url = "https://api.nasa.gov/planetary/apod?api_key=AcFc5Ztx2UpVK9YSh1jkM9iuvvSRCWwLEW9O90RB";
-  url = url + "&date=";
-  var date = $("demo").val();
+  var date = $("demo");
   console.log(date);
   e.preventDefault();
 })
 $("#demo").dcalendarpicker(
   {
     format: 'yyyy-mm-dd'
-  }
-);
-
+  }).on('datechanged', function(e){
+      var url = "https://api.nasa.gov/planetary/apod?api_key=AcFc5Ztx2UpVK9YSh1jkM9iuvvSRCWwLEW9O90RB";
+      url = url + "&date=" + e.date;  
+      console.log(url);
+      $.ajax({
+        url: url,
+        success: function(result){
+        if("copyright" in result) {
+          $("#copyright").text("Image Credits: " + result.copyright);
+        }
+        else {
+          $("#copyright").text("Image Credits: " + "Public Domain");
+        }
+        
+        if(result.media_type == "video") {
+          $("#apod_img_id").css("display", "none"); 
+          $("#apod_vid_id").attr("src", result.url);
+        }
+        else {
+          $("#apod_vid_id").css("display", "none"); 
+          $("#apod_img_id").attr("src", result.url);
+        }
+        $("#reqObject").text(url);
+        // $("#returnObject").text(JSON.stringify(result, null, 4));  
+        $("#apod_explaination").text(result.explanation);
+        $("#apod_title").text(result.title);
+      }
+      });
+    });
 });
